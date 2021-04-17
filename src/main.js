8cea1ad002fcf57deb6620fe5938c64924feb94a
Damien Jiang <damieneskimo@gmail.com>
2021-04-17 15:55:06 +1200

    add vue router for handling different view

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/patients/:id/notes', name: 'notes', component: Notes },
+  { path: '/', name: 'home', component: Home }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes // short for `routes: routes`
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
