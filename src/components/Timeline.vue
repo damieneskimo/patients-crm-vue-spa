900c27d05d02542a94336b5a07e40261f2f61444
Damien Jiang <damieneskimo@gmail.com>
2021-04-28 15:20:15 +1200

    show help text if no notes data

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="w-full px-20 py-8 bg-gray-200 overflow-hidden">
-    <ul class="list-none">
-        <li v-for="item in data" :key="item.id">
-            <div class="flex items-center">
-                <div class="border-r-2 border-green-400 py-8 px-4 h-full w-1/4 relative flex items-center justify-end">
-                    <p class="mr-5">{{ item.created_at }}</p>
-                    <div class="bg-green-500 rounded-full h-8 w-8 absolute -right-4"></div>
+    <div class="w-full px-20 py-8 bg-gray-200 overflow-hidden">
+        <ul v-if="data.length > 0" class="list-none">
+            <li v-for="item in data" :key="item.id">
+                <div class="flex items-center">
+                    <div class="border-r-2 border-green-400 py-8 px-4 h-full w-1/4 relative flex items-center justify-end">
+                        <p class="mr-5">{{ item.created_at }}</p>
+                        <div class="bg-green-500 rounded-full h-8 w-8 absolute -right-4"></div>
+                    </div>
+                    <div class="text-left w-3/4 ml-10">
+                        {{ item.content }}
+                    </div>
                 </div>
-                <div class="text-left w-3/4 ml-10">
-                    {{ item.content }}
-                </div>
-            </div>
-        </li>
-    </ul>
+            </li>
+        </ul>
+        <p v-else>There is no data found.</p>
     </div>
 </template>
 
